0f49bea581e9db4532bb32da9b56fe15b92cf4ea
Jacob Fisher <fisherjc2008@gmail.com>
2024-02-01 20:12:24 -0500

    populate autonomous mode types

--- src/main/java/frc/robot/enums/AutonomousModes.java
+++ src/main/java/frc/robot/enums/AutonomousModes.java
@@ -1,14 +1,27 @@
 package frc.robot.enums;
-/*
- * public enum AutonomousModes { EXAMPLE_AUTO(0, "Example Autonomous"); //
- * Position 1
- * 
- * private int id; private String friendlyName;
- * 
- * private AutonomousModes(int id, String friendlyName) { this.id = id;
- * this.friendlyName = friendlyName; }
- * 
- * public int getId() { return this.id; }
- * 
- * @Override public String toString() { return friendlyName; } }
- */
+
+public enum AutonomousModes
+    {
+    PASS_START_LINE(1, "Pass Start Line"), SCORE_AMP(2,
+            "Pass Start Line & Score Amp");
+
+    private int id;
+    private String friendlyName;
+
+    private AutonomousModes(int id, String friendlyName)
+        {
+            this.id = id;
+            this.friendlyName = friendlyName;
+        }
+
+    public int getId()
+    {
+        return this.id;
+    }
+
+    @Override
+    public String toString()
+    {
+        return friendlyName;
+    }
+    }
