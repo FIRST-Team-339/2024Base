6d1a7947236a5bc866469ad634f3147f60c325ab
Jacob Fisher <fisherjc2008@gmail.com>
2024-02-21 10:05:48 -0500

    dashboard: declare methods

--- src/main/java/frc/robot/subsystems/DashboardSubsystem.java
+++ src/main/java/frc/robot/subsystems/DashboardSubsystem.java
@@ -4,6 +4,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.enums.AutonomousModes;
 
 public class DashboardSubsystem extends SubsystemBase
     {
@@ -17,6 +18,26 @@ public class DashboardSubsystem extends SubsystemBase
         {
             SmartDashboard.putData("Field", field);
         }
+
+    public boolean getAutonomousEnabled()
+    {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    public AutonomousModes getAutonomousMode()
+    {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    public double getAutonomousDelay()
+    {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    public boolean getDemoModeEnabled()
+    {
+        throw new UnsupportedOperationException("Not implemented");
+    }
 
     public void updatePose(final Pose2d newPose)
     {
