91cb1ed1cf358b50734f08675da8886c3863bbe7
Robert Brown <rjbrown124@comcast.net>
2024-02-29 19:29:13 -0500

    fix joystick deadband
    Co-authored-by: Fisher <72705185+fisherjacobc@users.noreply.github.com>

--- src/main/java/frc/robot/constants/CurrentConstants.java
+++ src/main/java/frc/robot/constants/CurrentConstants.java
@@ -31,7 +31,7 @@ public final class CurrentConstants
             { false, true };
 
         /* JOYSTICK DEADBAND */
-        public static final double JOYSTICK_DEADBAND = 0.05;
+        public static final double JOYSTICK_DEADBAND = 0.2;
 
         /* Gears */
         public static final DriveGears DEFAULT_GEAR = DriveGears.GEAR1;
