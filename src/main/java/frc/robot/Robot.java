9111bc499c6dc859781bd0a9b562f262d9fa1de9
Robert Brown <rjbrown124@comcast.net>
2024-03-20 18:19:45 -0400

    chore: remove references to pose and differential drive

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -41,10 +41,6 @@ public class Robot extends TimedRobot
         DashboardSubsystem.ListenerType.AutonomousModeOption,
         this::onAutonomousModeOptionUpdate);
 
-    /* Initialize the Pose2d */
-    robotContainer.dashboardSubsystem
-        .updatePose(robotContainer.tankSubsystem.getPose());
-
     /* Initialize the AprilTag Detector */
     aprilTagThread = new Thread(
         AprilTagModule.getDetectorRunnable(robotContainer.cameraSubsystem));
@@ -75,12 +71,7 @@ public class Robot extends TimedRobot
     // and running subsystem periodic() methods. This must be called from the
     // robot's periodic
     // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
-
-    /* Update the Pose2d */
-    robotContainer.dashboardSubsystem
-        .updatePose(robotContainer.tankSubsystem.getPose());
-        
+    CommandScheduler.getInstance().run();        
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
