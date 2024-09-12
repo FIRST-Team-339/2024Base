7e548ccb94846ac9a3287ca80963cd48ac76a32a
Kilroy Programming <178730847+KilroyProgramming@users.noreply.github.com>
2024-09-12 19:16:14 -0400

    added shooter constants

--- src/main/java/frc/robot/constants/CurrentConstants.java
+++ src/main/java/frc/robot/constants/CurrentConstants.java
@@ -117,9 +117,10 @@ public final class CurrentConstants {
     public static final class ShooterConstants {
         /* Motor IDs */
         public static final int TOP_MOTOR_ID = 22;
-
+        public static final int BOTTOM_MOTOR_ID = 26;
 
         /* Speeds */
-        
+        public static final double INTAKE_SPEED = 0.25;
+        public static final double OUTTAKE_SPEED = 1.0;
     }
 }
